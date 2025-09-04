18485a807d60a9dc5867568f96556ac8dc88f7c4
earancibian <earancibia@tisal.cl>
2025-09-04 09:53:06 -0400

    variables de entorno

--- api/server.js
+++ api/server.js
@@ -9,6 +9,23 @@ app.use(cors());
 app.use(express.static('static'));
 app.use(express.json());
 
+const dbHost = process.env.DB_HOST;
+const state = { counter: 0 };
+app.get('/config', async (req, res) => {
+
+    res.status(200).json({ message: `Database Host: ${dbHost}` });
+
+});
+
+app.get('/healthcheck', async (req, res) => {
+
+   state.counter++;
+
+    console.log(`Healthcheck called ${state.counter} times`);
+
+  res.status(200).json({ status: 'ok' });
+
+});
 
 app.get('/endpoint', async (req, res) => {
   console.log('API is working');
